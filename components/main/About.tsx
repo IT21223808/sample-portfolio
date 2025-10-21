9706693986b4e1bc9f5e385b1646bcc6844a1b67
IT21223808 <IT21223808@my.sliit.lk>
2025-10-21 17:17:41 +0530

    commit

--- components/main/About.tsx
+++ components/main/About.tsx
@@ -3,9 +3,15 @@ import AboutContent from '../sub/AboutContent'
 
 const About = () => {
     return (
+        <section
+        id="about"
+        className="flex flex-col items-center justify-center gap-3 h-full relative overflow-hidden pb-80 py-20"
+        style={{ transform: "scale(0.9" }}
+      >
         <div className='relative flex flex-col h-full w-full'>
                 <AboutContent />
             </div>
+            </section>
     )
 }
 
